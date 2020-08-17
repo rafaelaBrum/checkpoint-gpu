50cd29a579cae74be08ad83172320fa9941a1cd5
Rafaela Brum <rafinhacbrum@gmail.com>
2020-08-17 09:00:36 -0300

    Adding the test repository

---
+++ test/autotest_config.py
@@ -0,0 +1,59 @@
+USE_TEST_SUITE="yes"
+USE_M32=0
+DEBUG="no"
+HBICT_DELTACOMP="no"
+ARM_HOST="no"
+
+# We may be running a user's python, but we should only test with canonical one
+HAS_PS="yes"
+HAS_PYTHON="yes"
+HAS_READLINE="no"
+HAS_DASH="yes"
+HAS_TCSH="no"
+HAS_ZSH="no"
+HAS_VIM="yes"
+VIM="/usr/bin/vim"
+HAS_EMACS="no"
+HAS_EMACS_NOX="no"
+HAS_SCRIPT="yes"
+HAS_SCREEN="yes"
+SCREEN="/usr/bin/screen"
+HAS_STRACE="yes"
+HAS_GDB="yes"
+HAS_JAVA="yes"
+HAS_JAVAC="no"
+HAS_SSH_LOCALHOST="no"
+HAS_CMA="yes"
+HAS_EPOLL_CREATE1="yes"
+HAS_CILK="no"
+HAS_GCL="no"
+GCL="no"
+HAS_MATLAB="no"
+MATLAB="no"
+TEST_POSIX_MQ="yes"
+HAS_MUTEX_WRAPPERS="no"
+
+OPENMP_CFLAGS="-fopenmp"
+if OPENMP_CFLAGS != "":
+  HAS_OPENMP="yes"
+else:
+  HAS_OPENMP="no"
+
+HAS_MPICH="no"
+MPICH_MPD=""
+MPICH_MPDBOOT=""
+MPICH_MPDALLEXIT=""
+MPICH_MPIEXEC=""
+MPICH_MPDCLEANUP=""
+
+# USES_OPENMPI_ORTED="@USES_OPENMPI_ORTED@"
+HAS_OPENMPI="yes"
+OPENMPI_MPICC="/usr/bin/mpicc.openmpi"
+OPENMPI_MPIRUN="/usr/bin/mpirun.openmpi"
+
+if USE_M32:
+  HAS_READLINE="no"
+  HAS_MPICH="no"
+  HAS_OPENMPI="no"
+  HAS_CILK="no"
+
